6ee4645878607a2835421d8d7557d048b181a9d3
Luis Roberto Paz Esparza <luissouspaz@hotmail.com>
2025-11-04 20:20:27 -0600

    Dejar de rastrear carpetas .idea y .venv

--- .venv/configuration.py
+++
@@ -1,6 +0,0 @@
-URL_SERVICE = ('https://cnt-575de42a-c4de-42db-b1c8-7edc2a9456ca.containerhub.tripleten-services.com')
-DOC_PATH = "/docs/"
-LOG_MAIN_PATH = "/api/logs/main/"
-USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
-CREATE_USER_PATH = "/api/v1/users/"
-PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
